46d43bae34fc3629731ab9a1459cafe0a25987a7
OleksandrShcherbinin <shcherbininalex81@gmail.com>
2019-10-28 17:10:42 +0200

    Total commit after intensive. Big data: media/images, logs, cache are git ignored

--- ForestGump/settings.py
+++ ForestGump/settings.py
@@ -89,16 +89,6 @@ CACHES = {
         'LOCATION': os.path.join(BASE_DIR, 'django_cache')
     }
 }
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'ForestGumpDB',
-#        'USER': 'Alex',
-#        'PASSWORD': 'Alexmvp5',
-#        'HOST': '127.0.0.1',
-#        'PORT': '5432',
-#    }
-#}
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
